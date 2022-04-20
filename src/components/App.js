c42e6177d360034f6f5b430bab910375a5acdf1b
“CarlotaBlanco” <lotsilots@gmail.com>
2022-04-20 11:04:58 +0200

    add errors button

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
-import logo from '../images/logo.svg';
-
 import '../styles/App.scss';
+import { useState } from 'react';
 
 function App() {
+  const [numberOfErrors, setNumberOfErrors] = useState(0);
+  const handleButton = (event) => {
+    event.preventDefault();
+    setNumberOfErrors(numberOfErrors + 1);
+  };
   return (
     <div className='page'>
       <header>
@@ -49,7 +53,7 @@ function App() {
             />
           </form>
         </section>
-        <section className='dummy error-5'>
+        <section className={`dummy error-${numberOfErrors}`}>
           <span className='error-13 eye'></span>
           <span className='error-12 eye'></span>
           <span className='error-11 line'></span>
@@ -64,6 +68,9 @@ function App() {
           <span className='error-2 line'></span>
           <span className='error-1 line'></span>
         </section>
+        <form action=''>
+          <button onClick={handleButton}>Incrementar</button>
+        </form>
       </main>
     </div>
   );
